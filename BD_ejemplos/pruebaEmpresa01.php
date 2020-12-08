fa6b2cbc2946c2ef73f7985e99c51aa91b596842
albertoinfo <alberto.lopez@iestetuan.es>
2020-12-08 22:51:38 +0100

    ejemplos BD mejorados

--- BD_ejemplos/pruebaEmpresa01.php
+++ BD_ejemplos/pruebaEmpresa01.php
@@ -1,22 +1,25 @@
 <?php
+/*
+ *  Ejemplo básico de consulta a la base de datos Empresa
+ */
 echo " Conectando a la base de datos <br>";
-$conex = new mysqli("192.168.105.94", "root", "root", "Empresa"); // Abre una conexión
+$conex = new mysqli("localhost", "root", "root", "Empresa"); // Abre una conexión
 if (mysqli_connect_errno()) {
     // Comprueba conexión
     printf("Conexión fallida: %s\n", mysqli_connect_error());
     exit();
 }
 $query = "SELECT EMP_NO,APELLIDO FROM EMPLEADOS";
+// Sí hay resultados
 if ($result = $conex->query( $query)) {
-    // Sí hay resultados
+    // Apunta a la primera fila, no es necesaria en este caso
     $result->data_seek(0);
+    // Extrae fila apuntada y apunta a la siguiente
+    // Obtiene un array asociativo
     while ( $fila = $result->fetch_assoc()) {
-        // Apunta a la primera fila
-        // Extrae fila apuntada y apunta a la siguiente
-        printf ("Nº: %s Apellido: %s <br>", $fila['EMP_NO'], $fila['APELLIDO'] );
         // Muestra sus datos
+        printf ("Nº: %s Apellido: %s <br>", $fila['EMP_NO'], $fila['APELLIDO'] );
     }
-   
     $result->close(); // libera recursos de la consulta
 }
 $conex->close(); // cierra conexión
--- BD_ejemplos/pruebaEmpresa02.php
+++ BD_ejemplos/pruebaEmpresa02.php
@@ -1,6 +1,16 @@
 <?php
+/**
+ *  Ejemplos de distintas maneras de obtener los datos de una
+ *  consulta.
+ *  - Array Asociativo  fetch_assoc
+ *  - Array Posiciones  fetch_row 
+ *  - Array Asociativo o con posiciones fetch_array
+ *  - Array de objetos  fetch_object 
+ *  
+ */
+
 echo " Conectando a la base de datos <br>";
-$conex = new mysqli("192.168.1.39", "root", "root", "Empresa"); // Abre una conexión
+$conex = new mysqli("localhost", "root", "root", "Empresa"); // Abre una conexión
 if ($conex->connect_errno) {
     // Comprueba conexión
     printf("Conexión fallida: %s\n", mysqli_connect_error());
@@ -24,7 +34,7 @@ if ($result = $conex->query( $query)) {
     echo "<hr>";
     $result->data_seek(0); // Voy a al inico 
     echo "<table border=1><th>Nº</th><th>Apellido</th><th>Salario</tr>";
-    while ( $fila = $result->fetch_array() ) {
+    while ( $fila = $result->fetch_row() ) {
         echo "<tr>";
         echo "<td>$fila[0]</td>";
         echo "<td>$fila[1]</td>";
@@ -33,7 +43,21 @@ if ($result = $conex->query( $query)) {
     }
     echo "</table>";
     
-    // Un objeto con con atributos con el valor del campo
+    // Array Asociativo por posición o por nombre
+    echo "<hr>";
+    $result->data_seek(0); // Voy a al inico
+    echo "<table border=1><th>Nº</th><th>Apellido</th><th>Salario</tr>";
+    while ( $fila = $result->fetch_array() ) {
+        echo "<tr>";
+        echo "<td>$fila[0]</td>";
+        echo "<td>$fila[APELLIDO]</td>";
+        echo "<td>$fila[2]</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    
+    //  Array de objetos con con atributos con el valor del campo
+    //  En este caso no indicamos la clase del objeto que se crea (stdClass)
     echo "<hr>";
     $result->data_seek(0); // Voy a al inico
     echo "<table border=1><th>Nº</th><th>Apellido</th><th>Salario</tr>";
